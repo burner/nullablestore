4be5017013705d236ee475141624656cdf72c92d
Robert Schadek <rschadek@symmetryinvestments.com>
2019-03-29 16:33:53 +0000

    init

---
+++ source/nullablestore.d
@@ -0,0 +1,48 @@
+module nullablestore;
+
+@safe pure nothrow @nogc:
+
+/** A very stupid type that does nothing more than store a `TypeValue` and a
+key. Is is useful to break cycles in type systems.
+The value of KeyType.init must equal nullvalue;
+*/
+struct NullableStore(T,KType = ulong, KType nullvalue = KType.init) {
+	///
+	alias TypeValue = T;
+	alias KeyType = KType;
+	static immutable NullValue = nullvalue;
+
+	///
+	long key;
+
+	bool isNull() const {
+		return this.key == NullValue;
+	}
+	
+	static assert(KeyType.init == NullValue);
+}
+
+version(unittest) {
+private:
+	struct Foo {
+		NullableStore!Bar bar;
+	}
+	
+	struct Bar {
+		Foo foo;
+	}
+}
+
+unittest {
+	Bar b;
+	Foo f;
+	static assert(NullableStore!(Bar).NullValue == ulong.min);
+	assert(f.bar.isNull());
+
+	static if(is(typeof(Foo.bar) : NullableStore!F, F)) {
+		assert(is(F == Bar));
+		assert(is(Foo.bar.TypeValue == Bar));
+	} else {
+		assert(false);
+	}
+}
